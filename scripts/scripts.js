c27065320e68ff57c041808d8a85ea103f18b2d1
Yrovi <angelopulhin21@gmail.com>
2025-06-09 22:17:57 +0800

    fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,7 @@ var clicks = 0;
 
 function showRandomQueenImage() {
             let pickedRandomQueenImageNum;
-            pickedRandomQueenImageNum = Math.floor(Math.random() * 15) + 1;
+            pickedRandomQueenImageNum = Math.floor(Math.random() * 14) + 1;
             let randomQueenImageFile;
             randomQueenImageFile = "queen/queen" + pickedRandomQueenImageNum + ".jpg";
             let randomQueenImage = document.getElementById("randomQueenImage");
